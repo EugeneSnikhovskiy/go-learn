8457789d8b56ce983c9ebdc20d4ad7ed17ed9ed1
Eugene Snikhovskiy <e.snikhovskiy@spellsystems.com>
2022-12-16 17:47:24 +0300

    HEAD

---
+++ less2.go
@@ -0,0 +1,34 @@
+package main;
+import "fmt";
+
+func main() {
+    printMessage("Hello world");
+    printMessage(sayHello("Eugene", 34));
+    // _ сбросит переменную
+    var message1, _ = checkAge(18);
+    var message2, _ = checkAge(46);
+    fmt.Println(message1);
+    fmt.Println(message2);
+    fmt.Println(checkAge(17));
+}
+
+func printMessage(message string) {
+    fmt.Println(message);
+}
+
+func sayHello(name string, age int) string {
+    // как конкатинировать строки и числа
+    var message = fmt.Sprintf("Hello, %s! Your age is %d years", name, age);
+    return message;
+}
+
+func checkAge(age int) (string, bool){
+    if (age >= 18 && age <= 45) {
+        var message = "You are middle";
+        return message, true;
+    } else if (age > 45) {
+        return "You are old", true;
+    }
+
+    return "You are yong", false;
+}
---
+++ notes.md
@@ -0,0 +1,8 @@
+// dep - версирование последняя актуальная
+// vgo - версирование минимальная версия и не выше
+// delve дебаггер для го
+
+// vim-go goland vs code atom-go
+
+// досмотреть общее https://youtu.be/2fxNbhy2gt0?t=1029
+// досмотреть базу https://youtu.be/h0zxh2TPN_I?t=4383
