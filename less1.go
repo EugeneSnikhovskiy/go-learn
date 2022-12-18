14bb37b3ae1bfaa29dae31b2f09572f9cdb7254a
eugene <eugene@eugenes-MacBook-Pro.local>
2022-12-18 19:16:57 +0300

    p1

--- less1.go
+++ less1.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"fmt"     // либа, аналог console.log в
 	"reflect" // либа аналог typeof в javascript
 )
 
-var a, b, c int;
+var a, b, c int
 
 func main() {
-    a, b, c := 1, 2, 3;
-    a, b = b, a; // поменять значение переменных местами
-    a, _, c = 5, 6, 7; // _ заигнорим множественное присвоение
+	a, b, c := 1, 2, 3
+	a, b = b, a       // поменять значение переменных местами
+	a, _, c = 5, 6, 7 // _ заигнорим множественное присвоение
 
-    var intVar int = 2;
-    var float1Var float32 = 22.55;
-	var a1 int = 2;
-	const msg string = "Hello";
-	var msg2 string = "HI";
-	message := "Hello world";
-    fmt.Println(float1Var, intVar, msg, msg2, message, a1);
-    fmt.Println(reflect.TypeOf(message));
-    fmt.Println(a, b, c);
-    print();
+	var intVar int = 2
+	var float1Var float32 = 22.55
+	var a1 int = 2
+	const msg string = "Hello"
+	var msg2 string = "HI"
+	message := "Hello world"
+	fmt.Println(float1Var, intVar, msg, msg2, message, a1)
+	fmt.Println(reflect.TypeOf(message))
+	fmt.Println(a, b, c)
+	print()
 }
 
 func print() {
-    // сошлется на глобальные переменные и выведет начальные значения (нули)
-    fmt.Println(a, b, c);
+	// сошлется на глобальные переменные и выведет начальные значения (нули)
+	fmt.Println(a, b, c)
 }
--- notes.md
+++ notes.md
@@ -4,5 +4,19 @@
 
 // vim-go goland vs code atom-go
 
-// досмотреть общее https://youtu.be/2fxNbhy2gt0?t=1029
+
+// ресурсы обучающие
+https://tour.golang.org
+https://gobyexample.com
+https://learnXinYminutes.com/docs/go
+https://slack.golang-ru.com
+https://github.com/hH39797J/golang-videos-ru
+https://vc.cc/8qPP0h (go 2 draft)
+
+// подсмотреть стандартные структуры проектов
+https://github.com/golang-standards/project-layout
+
+
+// досмотреть структуру папок https://youtu.be/B0lV7I3FO4E?t=609
 // досмотреть базу https://youtu.be/h0zxh2TPN_I?t=4383
+// качать лайв кодинг https://www.codingame.com
