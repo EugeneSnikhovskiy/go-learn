60b8ed38538c0d96bb0f1b7ecee44147d2931686
Eugene Snikhovskiy <e.snikhovskiy@spellsystems.com>
2023-01-25 19:06:06 +0300

    p1

---
+++ lesson9 - matrix and cycles/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	matrix := [][]int {}
+	matrix = make([][]int, 10)
+	counter := 0
+	for x := 0; x < 10; x++ {
+		matrix[x] = make([]int, 10)
+		for y := 0; y < 10; y++ {
+			counter++
+			matrix[x][y] = counter;
+		}
+	}
+	print(matrix)
+
+	arr := []string {"1", "2", "3"}
+	// спец синтаксис прохода по массиву
+	for i := range arr {
+		fmt.Println(i, arr[i])
+	}
+}
+
+func print(arr [][]int)  {
+	fmt.Println(arr)
+}
